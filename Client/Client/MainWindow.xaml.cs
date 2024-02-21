bc1fab5dba2ffa5cb709e4297e91c77de65ca5b2
Mohamed <88803877+mohamedlemroudi@users.noreply.github.com>
2024-02-21 23:25:56 +0100

    Canvis corregits.

---
+++ Client/Client/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnEnviar_Click(object sender, RoutedEventArgs e)
+        {
+            string serverIP = txtServerIP.Text;
+            int serverPort = Int32.Parse(txtServerPort.Text);
+            
+            string fila = txtFila.Text;
+            string columna = txtColumna.Text;
+            byte[] message = Encoding.UTF8.GetBytes($"{fila},{columna},<EOF>");
+
+            IPHostEntry host = Dns.GetHostEntry(serverIP);
+            IPAddress ip = host.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ip, serverPort);
+
+            try
+            {
+                Socket socket = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(endPoint);
+                if (socket.RemoteEndPoint != null)
+                {
+                    Console.WriteLine("Socket client connected to: " + socket.RemoteEndPoint.ToString());
+                    int byteSend = socket.Send(message);
+
+                    byte[] messageReceived = new byte[byteSend];
+                    int byteReceived = socket.Receive(messageReceived);
+
+                    txtHistorico.Text += $"\nMessage from server: {Encoding.UTF8.GetString(messageReceived, 0, byteReceived)}";
+
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ SincrProcessosExamen/SincrProcessosExamen/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SincrProcessosExamen
+{
+    public class Program
+    {
+        private static int SEM_CAPACITY = 2;
+        private static SemaphoreSlim sem = new SemaphoreSlim(SEM_CAPACITY);
+        public static int fils;
+        public static int increment = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdueix el numero de fils:");
+            fils = Convert.ToInt32(Console.ReadLine());
+            Start();
+        }
+
+        static void Start() 
+        {
+            Thread[] threads = new Thread[fils];
+
+            for (var x = 0; x < fils; x++)
+            {
+                int j = x + 1;
+                threads[x] = new Thread(() => ProcessItem(j))
+                {
+                    Name = $"Th_{x + 1}"
+                };
+                threads[x].Start();
+            }
+
+            // Wait for all threads to finish
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine($"Increment: {increment}");
+        }
+
+        private static void ProcessItem(int X)
+        {
+            int N = new Random().Next(1, 5);
+            int sleepTime = N * 1000;
+
+            for (int i = 0; i < X; i++)
+            {
+                Console.WriteLine($"Soc el fil número {X}, estic al pas {i + 1} i m’espero {N} segons");
+                Thread.Sleep(sleepTime);
+                sem.Wait();
+                increment = increment + 1;
+                Thread.Sleep(sleepTime);
+                sem.Release();
+                Console.WriteLine("Thread: " + Thread.CurrentThread.Name + " has finished.");
+            }
+        }
+    }
+}
